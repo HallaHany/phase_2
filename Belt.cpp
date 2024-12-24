41833bd00db0963846184dc4ca1ae86996a24197
هلا هانى رمضان احمد <halla.ahmed04@eng-st.cu.edu.eg>
2024-12-24 22:16:45 +0200

    minor change

--- Belt.cpp
+++ Belt.cpp
@@ -24,12 +24,10 @@ void Belt::Apply(Grid* pGrid, Player* pPlayer)
 
 	///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
 
-
-	// == Here are some guideline steps (numbered below) to implement this function ==
-
-	// 1- Print a message "You have reached a belt. Click to continue ..." and wait mouse click
-
+// 1- Print a message "You have reached a belt. Click to continue ..." and wait mouse click
+	pGrid->GetOutput()->PrintMessage("You have reached a belt. click to continue ...");
 	// 2- Apply the belt's effect by moving the player to the endCellPos
+	pGrid->UpdatePlayerCell(pPlayer, *(new CellPosition(endCellPos.GetCellNum())));
 	//    Review the "pGrid" functions and decide which function can be used for that
 
 }
--- Flag.cpp
+++ Flag.cpp
@@ -33,10 +33,12 @@ void Flag::Apply(Grid* pGrid, Player* pPlayer)
 
 	///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
 	// == Here are some guideline steps (numbered below) to implement this function ==
-
 	// 1- Print a message "You have reached a flag. Click to continue ..." and wait mouse click
-
+	pGrid->GetOutput()->PrintMessage("You have reached a flag. click to continue ...");
+	int x, y;
+	pGrid->GetInput()->GetPointClicked(x, y);
 	// 2- Apply the flag's effect by ending the game
+	pGrid->SetEndGame(true);
 	//    Review the "pGrid" functions and decide which function can be used for that
 }
 
