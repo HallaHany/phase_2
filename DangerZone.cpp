2fb5eff7293b8d2d561e7d3c7594bb9ea1cb23dd
هلا هانى رمضان احمد <halla.ahmed04@eng-st.cu.edu.eg>
2024-12-25 02:43:59 +0200

    yaraaab

--- DangerZone.cpp
+++ DangerZone.cpp
@@ -1,13 +1,6 @@
 #include "DangerZone.h"
-<<<<<<< Updated upstream
-#include"Player.h"
-=======
-<<<<<<< HEAD
 #include"player.h"
-=======
-#include"Player.h"
->>>>>>> 83930bfa231addc6790eed84f7a3c0b550aede24
->>>>>>> Stashed changes
+
 
 
 DangerZone::DangerZone(const CellPosition & dangerZonePosition): GameObject(dangerZonePosition)
@@ -32,15 +25,6 @@ void DangerZone::Apply(Grid * pGrid, Player * pPlayer)
 	pGrid->GetOutput()->PrintMessage("You have reached a danger zone. Click to continue ...");
 
 	// 2- Apply the danger zone's effect by reducing the health of the player by 1 
-<<<<<<< Updated upstream
-	
-=======
-<<<<<<< HEAD
-
-=======
-	
->>>>>>> 83930bfa231addc6790eed84f7a3c0b550aede24
->>>>>>> Stashed changes
 	pPlayer->SetHealth((pPlayer->GetHealth() - 1));
 	// 3- Update the players info which is displayed (check Grid class and decide which function to use)
 	string info;
--- Flag.cpp
+++ Flag.cpp
@@ -31,16 +31,12 @@ Objectschoise Flag::GetType() const
 void Flag::Apply(Grid* pGrid, Player* pPlayer)
 {
 	///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
-<<<<<<< HEAD
+
 		// == Here are some guideline steps (numbered below) to implement this function ==
 		// 1- Print a message "You have reached a flag. Click to continue ..." and wait mouse click
-=======
+
 	// == Here are some guideline steps (numbered below) to implement this function ==
 	// 1- Print a message "You have reached a flag. Click to continue ..." and wait mouse click
-<<<<<<< Updated upstream
-=======
->>>>>>> 83930bfa231addc6790eed84f7a3c0b550aede24
->>>>>>> Stashed changes
 	pGrid->GetOutput()->PrintMessage("You have reached a flag. click to continue ...");
 	int x, y;
 	pGrid->GetInput()->GetPointClicked(x, y);
